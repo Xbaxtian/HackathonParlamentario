9175ff04eb63c6cb1c906cb1a9635e80a886a58a
xbaxtian <xbaxtian@hotmail.com>
2018-07-20 23:36:31 -0500

    web controller

---
+++ application/controllers/Web.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Web extends CI_Controller{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index(){
+		$dataView = array("view"=>'web/index',"data"=>array());
+		$this->load->view('layout',$dataView);
+	}
+}
+
+
+
+ ?>
