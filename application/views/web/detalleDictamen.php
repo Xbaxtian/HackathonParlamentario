c585db03ab1250cfa6c51a64394bb9aa92eb6893
noayanez <kmylow.10@gmail.com>
2018-07-21 00:50:21 -0500

    dictamen detallado v1

---
+++ application/views/web/detalleDictamen.php
@@ -0,0 +1,27 @@
+<div align="center">
+    <h3>Sumilla:</h3>
+    <input type="text" name="sumilla" style="border:0px" readonly>
+
+    <h3>Propuesto por:</h3>
+    <input type="text" name="sumilla" style="border:0px" readonly>
+
+    <h3>Fecha de debate:</h3>
+    <input type="date" name="sumilla" style="border:0px" value="2018-07-22" readonly>
+
+    <h3>Calificar:</h3>
+    <p class="clasificacion">
+        <input id="radio1" type="radio" name="estrellas" value="5"><!--
+        --><label for="radio1">★</label><!--
+        --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+        --><label for="radio2">★</label><!--
+        --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+        --><label for="radio3">★</label><!--
+        --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+        --><label for="radio4">★</label><!--
+        --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+        --><label for="radio5">★</label>
+    </p>
+
+    <input type="text" name="comentario" placeholder="dejanos tu comentario...">
+    <button type="button" name="button">Enviar</button>
+</div>
