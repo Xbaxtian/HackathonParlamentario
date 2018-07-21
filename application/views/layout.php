8229c31ce113bfb2e02ec4f447a4b78028d62d7e
xbaxtian <xbaxtian@hotmail.com>
2018-07-21 04:44:27 -0500

    web pagina principal

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="es">
 	<head>
@@ -5,6 +8,8 @@
 		<title>Layout</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="<?=base_url()?>public/css/style.css">
+		<!--Web css-->
+		<link rel="stylesheet" href="<?=base_url()?>public/css/web.css">
 		<!--Bootstrap.css-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 		<!--JQuery-->
@@ -13,6 +18,8 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<!--Bootstrap.js-->
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+		<!--Iconos-->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 	</head>
 	<body>
 		<header id="main-header">
@@ -26,14 +33,6 @@
 		<div class="container" id="principal-container">
 			<? $this->load->view($view)?>
 		</div>
-		<footer id="main-footer">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-auto mx-auto">
-						LambdaDev
-					</div>
-				</div>
-			</div>
-		</footer>
+		
 	</body>
 </html>
--- application/views/web/index.php
+++ application/views/web/index.php
@@ -1,1 +1,90 @@
-<h1>Web</h1>
+
+<section>
+	<div class="row">
+		<div class="col-auto mx-auto my-3">
+			<input type="text" class="form-control busca" placeholder="Buscar..">
+		</div>
+	</div>
+	<div class="row">
+		<div class="card col-sm-10 mx-3 mb-3 py-1" id="tablero">
+			<div class="row align-items-center py-2">
+				<div class="col-2"><strong>Cod</strong></div>
+				<div class="col-5"><strong>Titulo</strong></div>
+				<div class="col-3"><strong>Fecha de debate</strong></div>
+				<strong>Estadistica</strong>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+			<div class="row align-items-center py-2">
+				<div class="col-2"><span class="align-middle">00000</span></div>
+				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
+				<div class="col-3">06/06/06</div>
+				<button class="btn btn-primary ml-2"><i class="fas fa-chart-pie"></i></button>
+			</div>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto mx-auto my-2">
+			<button class="btn btn-danger">Suscribirse</button>
+		</div>
+	</div>
+</section>
