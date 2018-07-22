6d3d5b89258197bf1ef386f000936994eb624e4b
noayanez <kmylow.10@gmail.com>
2018-07-21 20:01:33 -0500

    index actualizado 8pm

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -7,78 +7,13 @@
 	</div>
 	<div class="row">
 		<div class="card tarjeta col-md-12 col-sm-10 mx-3 mb-3 py-1" id="tablero">
-			<div class="row align-items-center py-2">
+			<div class="row align-items-center py-2 cabecera">
 				<div class="col-2"><strong>Cod</strong></div>
 				<div class="col-5"><strong>Titulo</strong></div>
 				<div class="col-3"><strong>Fecha de debate</strong></div>
 				<strong class="ml-1">Opinión</strong>
 			</div>
-			<div class="row item align-items-center py-2 ">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
-			<div class="row item align-items-center py-2">
-				<div class="col-2"><span class="align-middle">00000</span></div>
-				<div class="col-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</div>
-				<div class="col-3">06/06/06</div>
-				<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>
-			</div>
+
 
 		</div>
 	</div>
@@ -91,11 +26,24 @@
 
 <script>
 	$(document).ready(function(){
-		$("#tablero .item div").on("click",function(){
-			window.location.href = "<?=base_url()?>detalleDictamen";
+		$.post("dictamen/listardictamenes",{},function(data){
+			var aux = '';
+			for(var i in data){
+				aux = '<div class="row item align-items-center py-2">\
+					<div class="col-2"><span class="align-middle">'+data[i].codigo+'</span></div>\
+					<div class="col-5">'+data[i].titulo+'</div>\
+					<div class="col-3">'+data[i].fec_debate+'</div>\
+					<button class="btn btn-dark ml-2"><i class="fas fa-chart-pie"></i></button>\
+				</div>';
+				$("#tablero").append(aux);
+			}
+			$("#tablero .item div").on("click",function(){
+				window.location.href = "<?=base_url()?>detalleDictamen";
+			});
+			$("#tablero .item button").on("click",function(){
+				window.location.href = "<?=base_url()?>estadisticas";
+			});
 		});
-		$("#tablero .item button").on("click",function(){
-			window.location.href = "<?=base_url()?>estadisticas";
-		});
+
 	});
 </script>
