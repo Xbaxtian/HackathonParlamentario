bd2f376aef12f4d3e69a2a1647f3dbd3da52ac70
Jonathan <jonathanperalta1893@gmail.com>
2018-07-21 21:08:48 -0500

    yeah yeah

--- application/models/DictamenModel.php
+++ application/models/DictamenModel.php
@@ -8,7 +8,7 @@ class DictamenModel extends CI_Model{
     }
 
     public function getDictamenes(){
-            $this->db->select('*');
+            $this->db->select('id_dictamen,id_usuario,id_tipo_dictamen,codigo,titulo,version,sumilla,DATE_FORMAT(fec_debate,"%d/%m/%y") as fecha,id_estado');
             $this->db->from('dictamenes');
             $query = $this->db->get();
             $result = $query->result_array();
@@ -47,7 +47,7 @@ class DictamenModel extends CI_Model{
         }
     }
     public function getDictamenid($id){
-        $this->db->select('*');
+        $this->db->select('id_dictamen,id_usuario,id_tipo_dictamen,codigo,titulo,version,sumilla,DATE_FORMAT(fec_debate,"%d/%m/%y") as fecha,id_estado');
         $this->db->from('dictamenes');
         $this->db->where('id_dictamen = '.$id);
         $query = $this->db->get();
